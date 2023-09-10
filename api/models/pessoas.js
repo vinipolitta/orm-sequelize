24e7b2e1ca1a4b14e0bfacbd52fdff64d5ae5af2
vinipolitta <vinicius.politta1@gmail.com>
2023-09-10 18:49:58 -0300

    Refinar e customizar validações de campos utilizando funções e JS puro

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -1,19 +1,48 @@
-'use strict'
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Pessoas = sequelize.define('Pessoas', {
-    nome: DataTypes.STRING,
-    ativo: DataTypes.BOOLEAN,
-    email: DataTypes.STRING,
-    role: DataTypes.STRING
-  }, { paranoid: true })
-  Pessoas.associate = function(models) {
+  const Pessoas = sequelize.define(
+    "Pessoas",
+    {
+      nome: {
+        type: DataTypes.STRING,
+        validate: {
+          funcaoValidadora: function (dado) {
+            if (dado.length < 3)
+              throw new Error("O campo nome deve ter mais de 3 caracteres");
+          },
+        },
+      },
+      ativo: DataTypes.BOOLEAN,
+      email: {
+        type: DataTypes.STRING,
+        validate: {
+          isEmail: {
+            args: true,
+            msg: "Dado do tipo e-mail invalido",
+          },
+        },
+      },
+      role: DataTypes.STRING,
+    },
+    {
+      paranoid: true,
+      defaultScope: {
+        where: { ativo: true },
+      },
+      scopes: {
+        todos: {
+          where: {},
+        },
+      },
+    }
+  );
+  Pessoas.associate = function (models) {
     Pessoas.hasMany(models.Turmas, {
-      foreignKey: 'docente_id'
-    }) 
+      foreignKey: "docente_id",
+    });
     Pessoas.hasMany(models.Matriculas, {
-      foreignKey: 'estudante_id'
-    })
-
-  }
-  return Pessoas
-}
+      foreignKey: "estudante_id",
+    });
+  };
+  return Pessoas;
+};
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -1,20 +1,32 @@
-const { Router } = require('express')
-const PessoaController = require('../controllers/PessoaController')
+const { Router } = require("express");
+const PessoaController = require("../controllers/PessoaController");
 
-const router = Router()
+const router = Router();
 
 router
-  .get('/pessoas', PessoaController.pegaTodasAsPessoas)
-  .get('/pessoas/:id', PessoaController.pegaUmaPessoa)
-  .post('/pessoas', PessoaController.criaPessoa)
-  .put('/pessoas/:id', PessoaController.atualizaPessoa)
-  .delete('/pessoas/:id', PessoaController.apagaPessoa)
-  .get('/pessoas/:estudanteId/matricula/:matriculaId',  PessoaController.pegaUmaMatricula)
-  .post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula)
-  .post('/pessoas/:id/restaura', PessoaController.restauraPessoa)
-  .put('/pessoas/:estudanteId/matricula/:matriculaId',  PessoaController.atualizaMatricula)
-  .delete('/pessoas/:estudanteId/matricula/:matriculaId',  PessoaController.apagaMatricula)
-  .post('/pessoas/:estudanteId/matricula/:matriculaId/restaura', PessoaController.restauraMatricula)
+  .get("/pessoas", PessoaController.pegaPessoasAtivas)
+  .get("/pessoas/todos", PessoaController.pegaTodasAsPessoas)
+  .get("/pessoas/:id", PessoaController.pegaUmaPessoa)
+  .post("/pessoas", PessoaController.criaPessoa)
+  .put("/pessoas/:id", PessoaController.atualizaPessoa)
+  .delete("/pessoas/:id", PessoaController.apagaPessoa)
+  .get(
+    "/pessoas/:estudanteId/matricula/:matriculaId",
+    PessoaController.pegaUmaMatricula
+  )
+  .post("/pessoas/:estudanteId/matricula", PessoaController.criaMatricula)
+  .post("/pessoas/:id/restaura", PessoaController.restauraPessoa)
+  .put(
+    "/pessoas/:estudanteId/matricula/:matriculaId",
+    PessoaController.atualizaMatricula
+  )
+  .delete(
+    "/pessoas/:estudanteId/matricula/:matriculaId",
+    PessoaController.apagaMatricula
+  )
+  .post(
+    "/pessoas/:estudanteId/matricula/:matriculaId/restaura",
+    PessoaController.restauraMatricula
+  );
 
-
-module.exports = router
+module.exports = router;
