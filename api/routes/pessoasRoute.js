0d6313e1761040c18680d6379ad286b373f262c0
vinipolitta <vinicius.politta1@gmail.com>
2023-09-10 10:18:38 -0300

    Finish ORM Sequelize

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -1,8 +1,25 @@
 const { Router } = require("express");
-const PessaController = require("../controllers/PessoaController.js");
+const PessoaController = require("../controllers/PessoaController.js");
 
 const router = Router();
 
-router.get("/pessoas", PessaController.pegaTodasAsPessoas);
+router.get("/pessoas", PessoaController.pegaTodasAsPessoas);
+router.get("/pessoas/:id", PessoaController.pegaUmaPessoa);
+router.post("/pessoas", PessoaController.criaPessoa);
+router.put("/pessoas/:id", PessoaController.atualizaPessoa);
+router.delete("/pessoas/:id", PessoaController.apagaPessoa);
+router.get(
+  "/pessoas/:estudanteId/matricula/:matriculaId",
+  PessoaController.pegaUmaMatricula
+);
+router.post("/pessoas/:estudanteId/matricula", PessoaController.criaMatricula);
+router.put(
+  "/pessoas/:estudanteId/matricula/:matriculaId",
+  PessoaController.atualizaMatricula
+);
+router.delete(
+  "/pessoas/:estudanteId/matricula/:matriculaId",
+  PessoaController.apagaMatricula
+);
 
 module.exports = router;
